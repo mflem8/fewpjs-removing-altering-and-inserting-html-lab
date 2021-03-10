de2c8aee387681d27e28bc3b2aec986b91e7b9df
mflem8 <matthewfleming8@gmail.com>
2021-03-09 22:51:02 -0500

    commit tree 418745ac4f06b93390c6e601b41cc061e89d4cd8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+let att = document.createAttribute("id");
+att.value = "victory";
+newHeader.setAttributeNode(att);
+newHeader.innerHTML = 'Matt is the champion';
+
+
